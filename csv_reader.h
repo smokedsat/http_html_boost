09af82de5edb86072bc6903309c4c092eb2d5998
Smokedsat <65419292+smokedsat@users.noreply.github.com>
2023-09-12 13:39:38 +0300

    Update csv_reader.h

--- csv_reader.h
+++ csv_reader.h
@@ -55,7 +55,9 @@ public:
     }
 
     // Determinating column types and column names in table from file
-    void determineColumnTypes() {
+    void 
+        determineColumnTypesAndNames() 
+    {
         for (const auto& column_data : table) {
             bool is_numeric = true;
             for (const std::string& value : column_data) {
@@ -69,19 +71,21 @@ public:
         }
     }
 
-    // Âûâîä òàáëèöû â êîíñîëü
-    void printTable() {
-        if (isTableRead) {
+    // Printing table to the console
+    void 
+        printTable() 
+    {
+        if (!table.isEmpty()) {
             size_t numRows = table[0].size();
 
-            // Âûâîäèì çíà÷åíèÿ ñ ôèêñèðîâàííîé øèðèíîé ñòîëáöà
+            // std::cout data with fixed width of row
             for (size_t i = 0; i < numRows; i++) {
                 for (size_t j = 0; j < table.size(); j++) {
                     if (i < table[j].size()) {
                         std::cout << std::setw(20) << table[j][i];
                     }
                     else {
-                        std::cout << std::setw(20) << " "; // Çàïîëíÿåì ïóñòûìè çíà÷åíèÿìè, åñëè êîëîíêà êîðî÷å
+                        std::cout << std::setw(20) << " "; // empty row with width 20 if size low 
                     }
                 }
                 std::cout << std::endl;
@@ -92,82 +96,119 @@ public:
         }
     }
 
-    // ×òåíèå CSV ôàéëà
-    void readCSV(const std::string& filename) {
-        char delimiter = ';';
+    // Reading CSV file
+    void 
+        readCSV(const std::string& filename) 
+    {
 
         std::ifstream csv_file(filename);
 
         if (!csv_file.is_open()) {
             std::cout << "File is not opened." << std::endl;
-            return; // Âîçâðàùàåìñÿ â ñëó÷àå îøèáêè îòêðûòèÿ ôàéëà
+            return; // REturn if isnt opened
         }
-
+        
+        setDelimeter(csv_file);
+        csv_file.seekg(0, std::ios::beg);
+        
         std::string header;
         std::getline(csv_file, header);
+        
         std::istringstream header_stream(header);
         std::string column_name;
 
         while (std::getline(header_stream, column_name, delimiter)) {
-            table.push_back({ column_name }); // Ñîçäà¸ì âåêòîð ñ îäíèì ýëåìåíòîì (íàçâàíèåì êîëîíêè)
+            table.push_back({ column_name }); // Creating vectors with first element = column name
         }
 
         std::string line;
-        size_t size = table.size();
 
         while (getline(csv_file, line)) {
             std::istringstream columns_stream(line);
             std::vector<std::string> data_from_columns;
             size_t counter = 0;
 
-            while (std::getline(columns_stream, column_name, delimiter) && (counter != size)) {
+            while (std::getline(columns_stream, column_name, DELIMETER)) {
                 if (column_name.empty()) {
-                    table[counter].push_back(EMPTY_ROW); // Äîáàâëÿåì êîíñòàíòó - åñëè çíà÷åíèå â òàáëèöå îòñóòñòâóåò
+                    table[counter].push_back(EMPTY_ROW); // Pushing EMPTY_ROW const if in file we find ;; means empty row
                 }
                 else {
-                    table[counter].push_back(column_name); // Äîáàâëÿåì çíà÷åíèå â ñîîòâåòñòâóþùóþ êîëîíêó
+                    table[counter].push_back(column_name); // Pushing data to compatible column
                 }
                 counter++;
             }
         }
-
-        isTableRead = true;
         csv_file.close();
+
+        determineColumnTypesAndNames(); 
     }
 
-    // Î÷èñòêà òàáëèöû
-    void clearTable() {
+    // Clearing table
+    void 
+        clearTable() 
+    {
         table.clear();
         column_names.clear();
         column_types.clear();
-        isTableRead = false;
     }
 
-    // Ïîëó÷åíèå ìåòàäàííûõ î ñòðóêòóðå äàííûõ
-    std::vector<std::string> getColumnNames() const {
-        return column_names;
+    std::vector<std::string> 
+        getColumnNames() const 
+    {
+        if (!column_names.empty())
+        {
+            return column_names;
+        }
+        else
+        {
+            return std::vector<std::string> {};
+        }
     }
 
-    std::vector<std::string> getColumnTypes() const {
-        return column_types;
+    std::vector<std::string> 
+        getColumnTypes() const 
+    {
+        if (!column_types.empty())
+        {
+            return column_types;
+        }
+        else
+        {
+            return std::vector<std::string> {};
+        }
+    }
+
+    // Getting columns/rows with name
+    std::vector<std::string> 
+        getColumnsWithName(const std::string& name)
+    {
+        std::vector<std::string> results;
+    
+    
+        for (int i = 0; i < table[0].size(); i++)
+        {
+            if (!table[i][0].compare(name))
+            {
+                for (int j = 1; j < table[i].size(); j++)
+                {
+                    results.push_back(table[i][j]);
+                }
+                return results;
+            }
+        }
+        return results;
     }
 
 private:
-    bool isNumber(const std::string& s) {
+
+    bool 
+        isNumber(const std::string& s) 
+    {
         std::istringstream iss(s);
         double number;
         iss >> number;
-        // Åñëè ïðåîáðàçîâàíèå ïðîøëî óñïåøíî è íå îñòàëîñü íåïðî÷èòàííûõ ñèìâîëîâ,
-        // òî ñ÷èòàåì ñòðîêó ÷èñëîì.
+        // If casting was successful and there is no symbols left, string is number
         return !iss.fail() && iss.eof();
     }
 };
 
-int main__() {
-    Table current(PATH);
-    current.readCSV(PATH);
-    current.determineColumnTypes(); // Îïðåäåëåíèå òèïîâ äàííûõ
-    current.printTable();
-    //current.clearTable(); // Î÷èñòêà äàííûõ ïîñëå èñïîëüçîâàíèÿ
-    return 0;
-}
