85f89b14fb94111b2f90a85438b51290d4d053ed
Smokedsat <65419292+smokedsat@users.noreply.github.com>
2023-09-14 05:07:39 +0300

    Update http_server_html.cpp
    Made database local in main with multy access from async http connection

--- http_server_html.cpp
+++ http_server_html.cpp
@@ -3,7 +3,6 @@
 #include <boost/beast/version.hpp>
 #include <boost/asio.hpp>
 #include <boost/algorithm/string.hpp>
-#include <boost/shared_ptr.hpp>
 
 #include <cstdlib>
 
@@ -20,20 +19,32 @@ namespace http = beast::http;
 namespace net = boost::asio;
 using tcp = boost::asio::ip::tcp;
 
-const std::string file = "clear_uploaded_file.csv";
 const unsigned short port = 7777;
 
+// Delete system webkit data from file. It is 4 lines at begin and 1 line at end. Need to delete because its a trash
 void erase_4_1_lines(const std::string& filename);
+
+struct Database : public std::enable_shared_from_this<Database>
+{
+    std::vector<std::string> results;
+    std::vector<std::string> file_names;
+    std::vector<Table> tables;
+
+    Table curr_table;
+
+    size_t countOfTables = 0;
+    int currentTableId = 0;
+};
 
 class http_connection : public std::enable_shared_from_this<http_connection>
 {
 public:
-    http_connection(tcp::socket socket)
-        : socket_(std::move(socket))
+    // Constructor to access database in main via shared_ptr
+    http_connection(tcp::socket socket, std::shared_ptr<Database> & sptr_database)
+    : socket_(std::move(socket))
     {
-       
+        this->sptr_database = sptr_database;
     }
-
     void start()
     {
         read_request();
@@ -47,24 +58,13 @@ private:
     http::response<http::dynamic_body> response_;
     net::steady_timer deadline_{ socket_.get_executor(), std::chrono::seconds(60) };
     
-    // Мне кажется, что проблема здесь, так как разные потоки, разные данные
-    std::vector<std::string> results;
-    std::vector<std::string> file_names;
-    std::vector<Table> tables;
+    std::shared_ptr<Database> sptr_database;
 
-    //boost::shared_ptr<Table> sptr_curr_table = boost::make_shared<Table>(curr_table);
-    Table curr_table;
-
-    size_t countOfTables = 0;
-    int currentTableId = 0;
-    
-    std::string file_path;
-
-    void read_request()
+private:
+    void 
+        read_request()
     {
         auto self = shared_from_this();
-        ///auto shared_tables = boost::make_shared<std::vector<Table>>(tables);
-
         
         http::async_read(
             socket_,
@@ -78,11 +78,30 @@ private:
             });
     }
 
-    void process_request()
+    void 
+        write_response()
+    {
+        auto self = shared_from_this();
+    
+        response_.content_length(response_.body().size());
+    
+        http::async_write(
+            socket_,
+            response_,
+            [self](beast::error_code ec, std::size_t)
+            {
+                
+                self->socket_.shutdown(tcp::socket::shutdown_send, ec);
+                self->deadline_.cancel();
+            });
+    }
+
+    void 
+        process_request()
     {
         response_.version(request_.version());
-        response_.keep_alive(true);
-
+        response_.keep_alive(false);
+    
         switch (request_.method())
         {
         case http::verb::get:
@@ -95,14 +114,14 @@ private:
             }
             else if (request_.target().find("/showtable") == 0)
             {
-                if (request_.body().size() > 0)
+                if (request_.target().size() > 11)
                 {
                     std::string target = request_.target().substr(11);
                     std::cout << target << std::endl;
-
-                    currentTableId = std::stoi(target); // stoi returns integer from string
+                    
+                    sptr_database->currentTableId = std::stoi(target); // stoi returns integer from string
                 }
-
+    
                 response_.result(http::status::ok);
                 response_.set(http::field::server, "Get request");
                 
@@ -111,13 +130,13 @@ private:
             }
             else if (request_.target().find("/search") == 0)
             {
-                if (tables.empty())
+                if (sptr_database->tables.empty())
                 {
                     // Создаем HTTP-ответ
                     response_.result(http::status::ok);
                     response_.set(http::field::server, "Get request");
                     response_.prepare_payload();
-
+    
                     beast::ostream(request_.body()) << "Tables is empty. Cant start /search.\n ";
                     create_response();
                 }
@@ -127,19 +146,19 @@ private:
                     {
                         // Получаем полный путь из URI запроса
                         std::string target_path = request_.target().substr(8);
-
+    
                         // Ищем параметр column_name в URL-адресе
                         size_t param_pos = target_path.find("column_name=");
-
+    
                         if (param_pos != std::string::npos)
                         {
                             // Найден параметр column_name, извлекаем его значение
                             size_t value_start = param_pos + strlen("column_name=");
                             size_t value_end = target_path.find("&", value_start);
                             std::string column_name_value = target_path.substr(value_start, value_end - value_start);
-
+    
                             // Выполняем поиск по значению column_name_value
-                            results = tables[currentTableId].getColumnsWithName(column_name_value);
+                            sptr_database->results = sptr_database->tables[sptr_database->currentTableId].getColumnsWithName(column_name_value);
                         }
                     }
                 }
@@ -161,24 +180,29 @@ private:
             if (request_.target() == "/upload")
             {
                 std::string file_data = beast::buffers_to_string(request_.body().data());
-
-                const std::string filename = "uploaded_file_" + std::to_string(countOfTables) + ".csv";
-
+    
+                const std::string filename = "uploaded_file_" + std::to_string(sptr_database->countOfTables) + ".csv";
+    
                 std::ofstream outfile(filename);
                 outfile << file_data;
                 outfile.close();
-
+    
                 erase_4_1_lines(filename);
                 std::string newfilename = "clear_" + filename;
-                curr_table.readCSV(newfilename);
-                tables.emplace_back(curr_table);
-                countOfTables = tables.size();
-
-                file_names.push_back(newfilename);
-
+                sptr_database->curr_table.readCSV(newfilename);
+                sptr_database->tables.emplace_back(sptr_database->curr_table);
+                sptr_database->curr_table.clearTable();
+                /*if (!sptr_database->tables.empty() && (sptr_database->countOfTables == 0))
+                {
+                   
+                }*/
+                sptr_database->countOfTables = sptr_database->tables.size();
+    
+                sptr_database->file_names.push_back(newfilename);
+    
                 response_.result(http::status::ok);
                 response_.set(http::field::server, "Post request");
-                beast::ostream(response_.body()) << "File was successfully uploaded.\n" << "Count of loaded files: " << file_names.size() << "\n";
+                beast::ostream(response_.body()) << "File was successfully uploaded.\n" << "Count of loaded files: " << sptr_database->file_names.size() << "\n";
                 response_.prepare_payload();
                 create_response();
             }
@@ -201,24 +225,25 @@ private:
         write_response();
     }
 
-    void create_response()
+    void 
+        create_response()
     {
         if (request_.target().find("/showtable") == 0)
         {
-            if (!tables.empty())
+            if (!sptr_database->tables.empty())
             {
-                if (request_.body().size() > 0)
+                if (request_.target().size() > 11)
                 {
                     std::string target = request_.target().substr(11);
                     std::cout << target << std::endl;
-
-                    currentTableId = std::stoi(target); // stoi returns integer from string
-                    if ((tables.size() < currentTableId))
+    
+                    sptr_database->currentTableId = std::stoi(target); // stoi returns integer from string
+                    if ((sptr_database->tables.size() < sptr_database->currentTableId))
                     {
-                        std::cout << "currentTableId < " << currentTableId << ". It setted to 0. " << std::endl;
-                        currentTableId = 0;
+                        std::cout << "currentTableId < " << sptr_database->currentTableId << ". It setted to 0. " << std::endl;
+                        sptr_database->currentTableId = 0;
                     }
-                    beast::ostream(response_.body()) << generateDynamicResponse(tables[currentTableId]);
+                    beast::ostream(response_.body()) << generateDynamicResponse(sptr_database->tables[sptr_database->currentTableId]);
                 }
                 else
                 {
@@ -244,25 +269,19 @@ private:
                 << "<h1> File upload </h1>\n"
                 << "<p> <a href=\"/main\">Return to Main Page</a></p>\n"
                 << "<form action=\"http://127.0.0.1:"
-                << port
+                << std::to_string(PORT);
+            beast::ostream(response_.body())
                 << "/upload\" method=\"post\" enctype=\"multipart/form-data\">\n"
                 << "<input type=\"file\" name=\"upload-file\">\n"
                 << "<input type=\"submit\" value=\"Upload\">\n"
                 << "</form>\n"
                 << "</body>\n"
                 << "</html>\n";
-
-            /*std::string filename = "clear_uploaded_file.csv";
-            
-            curr_table.readCSV(filename);
-            curr_table.setTableFilename(filename);
-
-            tables.push_back(curr_table);*/
         }
         else if (request_.target().find("/search") == 0)
         {
             response_.set(http::field::content_type, "text/html");
-            beast::ostream(response_.body()) << generateDynamicResponse(results);
+            beast::ostream(response_.body()) << generateDynamicResponse(sptr_database->results);
         }
         else if (request_.target().find("/main") == 0)
         {           
@@ -277,7 +296,7 @@ private:
                 << "<p>Welcome to the main page. Choose an action:</p>\n"
                 << "<ul>\n"
                 << "<li><a href=\"/upload\">Upload a file</a></li>\n";
-            if (countOfTables != 0)
+            if (sptr_database->countOfTables != 0)
             {
                 beast::ostream(response_.body()) << "<li><a href=\"/showtable\">Show table</a></li>\n";
             }
@@ -289,9 +308,9 @@ private:
                 << "    <input type=\"text\" id=\"column_name\" name=\"column_name\"><br><br>\n"
                 << "    <input type=\"submit\" value=\"Search\">\n"
                 << "</form>\n";
-
+    
             // Добавьте кнопку "SHOW COLUMN NAMES" для отображения имен столбцов
-            if (countOfTables != 0)
+            if (sptr_database->countOfTables != 0)
             {
                 beast::ostream(response_.body())
                     << "<form action=\"/main\" method=\"get\">\n"
@@ -299,18 +318,18 @@ private:
                     << "    <input type=\"submit\" value=\"SHOW COLUMN NAMES\">\n"
                     << "</form>\n";
             }
-
+    
             // Проверьте параметр запроса show_columns и выведите имена столбцов, если он установлен в "true"
-
+    
             if (request_.target().find("show_columns=true") != std::string::npos)
             {
-                if (!tables.empty())
+                if (!sptr_database->tables.empty())
                 {
                     // Получите список имен столбцов (например, из вашей таблицы)
-                    results = tables[currentTableId].getColumnNames();
-
+                    sptr_database->results = sptr_database->tables[sptr_database->currentTableId].getColumnNames();
+    
                     beast::ostream(response_.body()) << "<br><strong>Column Names:</strong><br><ul>";
-                    for (const std::string& result : results)
+                    for (const std::string& result : sptr_database->results)
                     {
                         beast::ostream(response_.body()) << "<li><strong>" << result << "</strong></li>";
                     }
@@ -321,28 +340,14 @@ private:
                     beast::ostream(response_.body()) << "<br><strong>Table is empty. Cant find column names.</strong><br><ul>";
                 }
             }
-
+    
             beast::ostream(response_.body())
                 << "</body>\n"
                 << "</html>\n";
         }
     }
 
-    void write_response()
-    {
-        auto self = shared_from_this();
-
-        response_.content_length(response_.body().size());
-
-        http::async_write(
-            socket_,
-            response_,
-            [self](beast::error_code ec, std::size_t)
-            {
-                self->socket_.shutdown(tcp::socket::shutdown_send, ec);
-                self->deadline_.cancel();
-            });
-    }
+    
 
     void check_deadline()
     {
@@ -359,7 +364,8 @@ private:
     }
     
     // HTML with vector<std::string> result
-    std::string generateDynamicResponse(const std::vector<std::string>& data)
+    std::string 
+        generateDynamicResponse(const std::vector<std::string>& data)
     {
         std::stringstream html;
         html << "<!DOCTYPE html>\n";
@@ -368,7 +374,7 @@ private:
         html << "    <title>RESULTS</title>\n";
         html << "</head>\n";
         html << "<body>\n";
-
+    
         if (data.empty()) {
             html << "<h1>table is empty</h1>\n";
         }
@@ -376,24 +382,25 @@ private:
             html << "<h1>RESULTS</h1>\n";
             html << "<table>\n";
             html << "<tr>\n";
-
+    
             // Создаем ячейки таблицы и заполняем их значениями из вектора
             for (const std::string& value : data) {
                 html << "<td>" << value << "</td>\n";
             }
-
+    
             html << "</tr>\n";
             html << "</table>\n";
         }
-
+    
         html << "</body>\n";
         html << "</html>\n";
-
+    
         return html.str();
     }
 
-    // HTML with result from Table
-    std::string generateDynamicResponse(const Table& table)
+        // HTML with result from Table
+    std::string 
+        generateDynamicResponse(const Table& table)
     {
         if (!table.table.empty())
         {
@@ -403,7 +410,7 @@ private:
             html << "<head>\n";
             html << "    <title>          ";
             html << table.table_filename;
-            html << "           < / title>\n";
+            html << "           </title>\n";
             html << "</head>\n";
             html << "<body>\n";
             html << "    <h1>                 </h1>\n";
@@ -450,9 +457,10 @@ private:
             return "";
         }
     }
-};
+}; // endof class http_connection
 
-void erase_4_1_lines(const std::string& filename)
+void 
+    erase_4_1_lines(const std::string& filename)
 {
     std::ifstream inputFile(filename);
     std::ofstream outputFile("clear_" + filename);
@@ -491,14 +499,15 @@ void erase_4_1_lines(const std::string& filename)
     std::filesystem::remove(filename);
 }
 
-void http_server(tcp::acceptor& acceptor, tcp::socket& socket)
+void 
+    http_server(tcp::acceptor& acceptor, tcp::socket& socket, std::shared_ptr<Database> & sptr_database)
 {
     acceptor.async_accept(socket,
         [&](beast::error_code ec)
         {
             if (!ec)
-                std::make_shared<http_connection>(std::move(socket))->start();
-            http_server(acceptor, socket);
+                std::make_shared<http_connection>(std::move(socket),sptr_database)->start();
+            http_server(acceptor, socket, sptr_database);
         });
 }
 
@@ -508,6 +517,10 @@ void http_server(tcp::acceptor& acceptor, tcp::socket& socket)
 
 int main(int argc, char* argv[])
 {
+    Database database_;
+    std::shared_ptr<Database> sptr_database_ = std::make_shared<Database>(database_);
+    
+
     if (argc != 3)
     {
         argc = 3;
@@ -534,7 +547,7 @@ int main(int argc, char* argv[])
 
         tcp::acceptor acceptor{ ioc, {address, port} };
         tcp::socket socket{ ioc };
-        http_server(acceptor, socket);
+        http_server(acceptor, socket, sptr_database_);
 
         ioc.run();
     }
@@ -543,4 +556,6 @@ int main(int argc, char* argv[])
         std::cerr << "Error: " << e.what() << std::endl;
         return EXIT_FAILURE;
     }
+    
+    return 0;
 }
