c1b424f000043d8a4d5d90e51f563c3c6a19384d
Smokedsat <65419292+smokedsat@users.noreply.github.com>
2023-09-15 16:10:25 +0300

    Create DynamicHTML.h

---
+++ DynamicHTML.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#include "csv_reader.h"
+#include <sstream>
+#include <vector>
+#include <string>
+
+// HTML with vector<std::string> result
+std::string generateDynamicResponse(const std::vector<std::string>& data, const std::string & ip_address, const int & port)
+{
+    std::stringstream html;
+    html << "<!DOCTYPE html>\n";
+    html << "<html>\n";
+    html << "<head>\n";
+    html << "    <title>RESULTS</title>\n";
+    html << "</head>\n";
+    html << "<body>\n";
+    html << "<p><strong> <a href=\"/main\">Return to Main Page</a></strong></p>\n";
+    html << "<form action=\"http://";
+    html << ip_address;
+    html << ":";
+    html << std::to_string(port);
+    html << "\">";
+    if (data.empty())
+    {
+        html << "<h1>Nothing to show</h1>\n";
+    }
+    else
+    {
+        html << "<h1>RESULTS</h1>\n";
+        html << "<table>\n";
+        html << "<tr>\n";
+
+        // Создаем ячейки таблицы и заполняем их значениями из вектора
+        for (const std::string& value : data) {
+            html << "<td>" << value << "</td>\n";
+        }
+
+        html << "</tr>\n";
+        html << "</table>\n";
+    }
+
+    html << "</body>\n";
+    html << "</html>\n";
+
+    return html.str();
+}
+
+// HTML with result from Table
+std::string generateDynamicResponse(const Table& table, const std::string& ip_address, const int& port)
+{
+    if (!table.table.empty())
+    {
+        std::stringstream html;
+        html << "<!DOCTYPE html>\n";
+        html << "<html>\n";
+        html << "<head>\n";
+        html << "    <title>          ";
+        html << table.table_filename;
+        html << "           </title>\n";
+        html << "</head>\n";
+        html << "<body>\n";
+        html << "    <h1>                 </h1>\n";
+        html << "    <table>\n";
+        html << "        <tr>\n";
+
+
+        // Output column names at 1 string. Something wrong
+        /*
+        for (const std::string& column_name : table.column_names) {
+            html << "            <th>" << column_name << "</th>\n";
+        }*/
+
+        html << "        </tr>\n";
+
+        for (const auto& rows : table.table) {
+            html << "        <tr>\n";
+            for (auto i = 0; i < rows.size() - 1; i++) {
+                if (!rows[i].empty() && (i != 0))
+                {
+                    html << "            <td>" << rows[i] << "</td>\n";
+                }
+                else if (!rows[i].empty() && (i == 0))
+                {
+                    html << "            <td><b>" << rows[i] << "</b></td>\n";
+                }
+                else
+                {
+                    html << "            <td></td>\n";
+                }
+            }
+            html << "        </tr>\n";
+        }
+
+        html << "    </table>\n";
+        html << "<p><strong> <a href=\"/main\">Return to Main Page</a></strong></p>\n";
+        html << "<form action=\"http://";
+        html << ip_address;
+        html << ":";
+        html << std::to_string(port);
+        html << "\">";
+        html << "</body>\n";
+        html << "</html>\n";
+
+        return html.str();
+    }
+    else
+    {
+        return "";
+    }
+}
